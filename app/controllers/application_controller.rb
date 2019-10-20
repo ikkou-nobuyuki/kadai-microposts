6f60c585d2757012589c8cf9bf002967a3ecfa39
ikkou.nobuyuki <ikko1550@gmail.com>
2019-10-20 06:56:30 +0000

    follow, unfollow

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,9 +4,15 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def require_user_logged_in
+   def require_user_logged_in
     unless logged_in?
       redirect_to login_url
     end
+   end
+
+  def counts(user)
+    @count_microposts = user.microposts.count
+    @count_followings = user.followings.count
+    @count_followers = user.followers.count
   end
 end
