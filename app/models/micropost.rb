8e2d5454c4c55f8ccd54c357d190dc562885421f
ikkou.nobuyuki <ikko1550@gmail.com>
2019-10-21 10:27:51 +0000

    kadai-favorite

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,4 +2,8 @@ class Micropost < ApplicationRecord
   belongs_to :user
   
   validates :content, presence: true, length: { maximum: 255 }
+  
+  # お気に入り機能の実装
+  has_many :favorites
+  has_many :be_liked, through: :favorites, source: :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,27 +7,52 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
+  # フォロー機能の実装
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
   
+  # お気に入り機能の実装
+  has_many :favorites
+  has_many :likes, through: :favorites, source: :micropost
+  
+  # フォローする
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
     end
   end
 
+  # フォローを外す
   def unfollow(other_user)
     relationship = self.relationships.find_by(follow_id: other_user.id)
     relationship.destroy if relationship
   end
 
+  # フォロー判定
   def following?(other_user)
     self.followings.include?(other_user)
   end
   
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+  
+  # お気に入り登録
+  def like(micropost)
+    favorites.find_or_create_by(micropost_id: micropost.id)
+  end
+  
+  # お気に入りから外す
+  def unlike(micropost)
+    favorate = favorites.find_by(micropost_id: micropost.id)
+    favorate.destroy if favorate
+  end
+  
+  #　お気に入り判定
+  def likes?(micropost)
+    self.likes.include?(micropost)
   end
 end
